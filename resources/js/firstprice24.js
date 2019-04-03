815bca33bb55da62fbe06a8ecf37bf09684dc9ec
Marvin Langer <info@motion-m.de>
2019-04-03 16:34:12 +0200

    added paypal financing updates

--- resources/js/firstprice24.js
+++ resources/js/firstprice24.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(function() {
 
 	jQuery(".search-input").attr("placeholder","Suchbegriff eingeben und Enter drücken...");
+
+		jQuery(".checkout .paypal-installment-specific-promotion").css("display","none");
+
+	function checkFinanzierungsanzeige(){
+		setTimeout(function(){
+			if( jQuery("#checkoutButtonPayPalInstallment").text() == "Ratenzahlung beantragen" ){
+				jQuery("#paypal-installment-specific-promotion_").css("display","block");
+			}else{
+				jQuery("#paypal-installment-specific-promotion_").css("display","none");
+			}
+		},500);
+	}
+
+	checkFinanzierungsanzeige();
+
+	$("body").on('DOMSubtreeModified', "#checkoutButtonPayPalInstallment", function() {
+	    checkFinanzierungsanzeige();
+	});
 	
 });
